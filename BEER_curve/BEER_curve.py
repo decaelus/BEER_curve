79642c31fd8bf10f91872d700846b2c084c1feb7
Brian Jackson <bjackson@boisestate.edu>
2018-05-16 14:58:48 -0600

    Removed import of batman package
    I can't seem to install Kreidburg's batman transit package via pip anymore. I'll try using PyAstronomy - https://pyastronomy.readthedocs.io.

--- BEER_curve/BEER_curve.py
+++ BEER_curve/BEER_curve.py
@@ -1,36 +1,35 @@
 import numpy as np
-from batman import *
+from PyAstronomy.modelSuite import forTrans as ft
 
 __all__ = ['BEER_params', 'BEER_curve']
 
-class BEER_params(TransitParams):
+class BEER_curve():
     """
-    Object in which to store the physical parameters of the system.
+    Calculates the BEaming, Ellipsoidal variation, and Reflected/emitted components
 
     :param Aellip: amplitude (unitless) of the ellipsoidal variations
-    :type u: float 
+    :type u: float
 
     :param Abeam: amplitude (unitless) of the beaming, RV signal
-    :type u: float 
+    :type u: float
 
     :param Aplanet: amplitude (unitless) of planet's reflected/emitted signal
-    :type u: float 
+    :type u: float
 
     """
-    def __init__(self, **kwargs):
-        super(TransitParams, self).__init__(**kwargs)
-        
+
+    def __init__(self, params, t):
         #Setting my own parameters
-        self.Aellip = None
-        self.Abeam = None
-        self.Aplanet = None
+        self.Aellip = params.Aellip
+        self.Abeam = params.Abeam
+        self.Aplanet = params.Aplanet
 
-class BEER_curve(object):
-    """
-        Calculates the BEaming, Ellipsoidal variation, and Reflected/emitted
-    """
+#   def planetary_curve(phi, A_refl):
+#       y = -(A_refl*cos(2.*pi*phi))
+#       y = y + abs(min(y))  
+#       return y
 
-    def planetary_curve(phi, A_refl):
-        y = - (A_refl*cos(2.*pi*phi))
-        y = y + abs(min(y))  
-        return y
+if __name__ == "__main__":
+    BC = BEER_curve()
+
+    print(BC.params)
