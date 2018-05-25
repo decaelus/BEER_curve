7b5f9700b4d1e5ba6c5ba4f592abde62cbac8289
Brian Jackson <bjackson@boisestate.edu>
2018-05-25 14:03:32 -0600

    Reverted to original downsample technique

--- BEER_curve/BEER_curve.py
+++ BEER_curve/BEER_curve.py
@@ -237,22 +237,12 @@ class BEER_curve(object):
         self.model_signal = full_signal
 
         if(self.supersample_factor > 1): 
-#           self.model_signal =\
-#                   np.mean(full_signal.reshape(-1, self.supersample_factor),\
-#                   axis=1)
-#           full_signal =\
-#                   np.mean(full_signal.reshape(-1, self.supersample_factor),\
-#                   axis=1)
-
-            # Make sure to bin the left-most data, too.
-            digitized = np.digitize(time_supersample, time)
-            print(digitized)
-            self.model_signal = np.array([
-                full_signal[digitized == i].mean() for i in range(0,
-                    len(time))])
-            full_signal = np.array([
-                full_signal[digitized == i].mean() for i in range(0,
-                    len(time))])
+            self.model_signal =\
+                    np.mean(full_signal.reshape(-1, self.supersample_factor),\
+                    axis=1)
+            full_signal =\
+                    np.mean(full_signal.reshape(-1, self.supersample_factor),\
+                    axis=1)
 
         return full_signal
 
@@ -321,6 +311,66 @@ class BEER_curve(object):
 
         return eclipse_bottom
 
+
+   def smooth(x, window_len=11, window='flat'):
+    """smooth the data using a window with requested size.
+
+    From http://scipy-cookbook.readthedocs.io/items/SignalSmooth.html
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; an odd integer
+        window: the type of window 
+            from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'.
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=numpy.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=numpy.ones(window_len,'d')
+    else:
+        w=eval('numpy.'+window+'(window_len)')
+
+    y=numpy.convolve(w/w.sum(),s,mode='valid')
+    return y 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
