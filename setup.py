2d85b7c1d93996027d1f1f689c4ed0705b63e066
Brian Jackson <decaelus@gmail.com>
2018-05-28 15:16:03 -0600

    Fixing

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup
 
-# read the contents of your README file
-# https://packaging.python.org/guides/making-a-pypi-friendly-readme/
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
-    long_description = f.read()
-
 def readme():
     with open('README.md') as f:
         return f.read()
@@ -18,14 +11,15 @@ setup(name='BEER_curve',
     author_email='bjackson@boisestate.edu',
     url='https://github.com/decaelus/BEER_curve',
     download_url = 'https://github.com/decaelus/BEER_curve/archive/0.1.tar.gz',
+    long_description = 'A very small package to model the BEaming, Ellipsoidal
+    variations, and Reflected/emitted light from low-mass companions',
+    long_description_content_type = 'text/plain',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Topic :: Scientific/Engineering',
         'Programming Language :: Python'
     ],
-    license=['GNU GPLv3'],
+    license=['MIT'],
     packages=['BEER_curve'],
     install_requires=['PyAstronomy', 'statsmodels'],
-    long_description=long_description,
-    long_description_content_type='text/markdown; charset=UTF-8; variant=GFM',
     zip_safe=True)
